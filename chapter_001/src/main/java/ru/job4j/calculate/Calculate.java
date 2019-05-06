d8018d2d227921ce4bfe9ae3ac8f6ebff09e9186
denis.ulyanov <d.ulyanov@yahoo.de>
2019-05-06 14:15:17 +0200

    1.3. JavaDoc. Code convention

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,21 @@
+package ru.job4j.calculate;
+
+/**
+* Class Класс для различных упражнений с Java
+* @author uljanow
+* @since 06.05.2019
+* @version 1
+*/
+
+public class Calculate {
+	
+	public static void main(String[] args){
+
+	System.out.println("Hello World");
+
+/**
+* Конструктор, вывод строки в консоль.
+* @param args - args.
+*/
+	}
+}
